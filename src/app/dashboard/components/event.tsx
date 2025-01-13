6545eb907e447805ce0ceb24acc84f98e0bb41b2
Arun Shrestha <arun.shrestha@vsic.edu.np>
2025-01-13 18:57:55 +0545

    fix fix

--- src/app/dashboard/components/event.tsx
+++ src/app/dashboard/components/event.tsx
@@ -4,7 +4,6 @@ import { useEffect, useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import ReactDOMServer from 'react-dom/server';
 import {
   Card,
   CardContent,
