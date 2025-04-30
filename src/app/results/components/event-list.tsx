1a35e0375d7a0e750c08958ec2f85d5128f0da15
Arun Shrestha <arun.shrestha@vsic.edu.np>
2025-04-30 11:39:58 +0545

    made public page dynamic and some changes

--- src/app/results/components/event-list.tsx
+++ src/app/results/components/event-list.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import EventDialog from "./event-dialogue"
 import { EventData, GroupAndSchedule } from "@/server/publicResult"
